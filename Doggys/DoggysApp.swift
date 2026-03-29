3092e01a6a318b8c149e8119645909948975084f
SofiVieira <sofiavieirarocha35@gmail.com>
2026-03-29 14:01:44 -0300

    Initial Commit

---
+++ Doggys/DoggysApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoggysApp.swift
+//  Doggys
+//
+//  Created by Sofia Vieira Rocha on 29/03/26.
+//
+
+import SwiftUI
+
+@main
+struct DoggysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
